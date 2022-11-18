30d68faa9fb7e531bd6edb54d465587854b0287d
15212010535 <2278663531@qq.com>
2022-11-18 10:57:52 +0800

    链表的中间结点

---
+++ linked_list/middleNode.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+	链表的中间结点
+*/
+
+func middleNode(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	p1 := head
+	p2 := head
+	for p2.Next != nil && p2.Next.Next != nil {
+		p1 = p1.Next
+		p2 = p2.Next.Next
+	}
+	// 判断奇偶
+	length := 0
+	cur := head
+	for cur != nil {
+		length++
+		cur = cur.Next
+	}
+	if length%2 == 0 {
+		return p1.Next
+	}
+	return p1
+}
